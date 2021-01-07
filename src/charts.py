78efe9a282423d63e33e0a6ed27e79a71695002d
MEGKRAMPIAN Arsak <arsak.megkrampian@inria.fr>
2021-01-07 16:07:34 +0100

    improve charts

--- src/charts.py
+++ src/charts.py
@@ -1,71 +1,66 @@
-import os
+import os, sys
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def PlotPriceDistribution(df=None):
+def PlotPriceDistribution(df=None, path=None):
 	"""
 	:param df: Pandas data frame
+	:param path: Path to the direcory where to save the chart
 	:creates the Price Distribution chart
 	:return: None
 	"""
-	df1 = df[df['UnitPrice']<1]
+	maxRows = 100
+	dataFrames = []
 
-	df2 = df[df['UnitPrice']>=1]
-	df2 = df2[df2['UnitPrice']<20]
+	try:
+		if not os.path.exists(path):
+			os.makedirs(path)
 
-	df3 = df[df['UnitPrice']>=20]
-	df3 = df3[df3['UnitPrice']<100]
+		while len(df) > maxRows:
+			top = df[:maxRows]
+			dataFrames.append(top)
+			df = df[maxRows:]
+		else:
+			dataFrames.append(df)
 
-
-	df4 = df[df['UnitPrice']>=100]
-	df4 = df4[df4['UnitPrice']<1000]
-
-	df5 = df[df['UnitPrice']>=1000]
-
-	path = "../charts/PriceDistribution/"
-	if not os.path.exists(path):
-		os.makedirs(path)
-
-	df1.plot(kind='line', x='UnitPrice', y='Sold Amount')
-	plt.savefig(path+"1_UnitPrice_1.jpg")
-
-	df2.plot(kind='line', x='UnitPrice', y='Sold Amount')
-	plt.savefig(path+"2_UnitPrice_20.jpg")
-
-	df3.plot(kind='line', x='UnitPrice', y='Sold Amount')
-	plt.savefig(path+"3_UnitPrice_100.jpg")
-
-	df4.plot(kind='line', x='UnitPrice', y='Sold Amount')
-	plt.savefig(path+"4_UnitPrice_1000.jpg")
-
-	df5.plot(kind='line', x='UnitPrice', y='Sold Amount')
-	plt.savefig(path+"5_UnitPrice_40000.jpg")
+		count = 1
+		for adf in dataFrames:
+			adf.plot(kind='line', x='UnitPrice', y='Sold Amount')
+			plt.savefig(path+str(count)+"_UnitPrice.jpg")
+			count = count+1
+	except Exception as e:
+		print(e)
+		sys.exit("Could not generate the price distribution charts")
 
 
-def PlotProductsDistributionPerCountries(df=None):
+def PlotProductsDistributionPerCountries(df=None, path=None):
 	"""
-	:param data: Pandas data frame
+	:param df: Pandas data frame
+	:param path: Path to the direcory where to save the chart
 	:creates the Products Distribution Per Countries chart
 	:return: None
 	"""
-	path = "../charts/ProductsDistributionPerCountries/"
-	if not os.path.exists(path):
-		os.makedirs(path)
+	try:
+		if not os.path.exists(path):
+			os.makedirs(path)
 
-	for index, row in df.iterrows():
-		country = row[0]
-		newDf = pd.DataFrame(row[1])
-		newDf.columns = ['StockCode', 'Sold Amount']
-		stockCodeList = (list(newDf['StockCode']))
-		soldAmount = (list(newDf['Sold Amount']))
-		fig, ax = plt.subplots(figsize =(140, 45))
-		ax.bar(stockCodeList, soldAmount)
-		ax.set_xlabel('StockCode',fontsize=100)
-		ax.set_ylabel('Sold Amount',fontsize=100)
-		plt.title('Products Distribution of ' + country, fontsize=100)
-		plt.xticks(rotation=90, fontsize=20)
-		plt.yticks(fontsize=50)
-		plt.savefig(path+country+".jpg", dpi=100)
-		plt.cla()
-		plt.clf()
-		plt.close(fig)
+		for index, row in df.iterrows():
+			country = row[0]
+			newDf = pd.DataFrame(row[1])
+			newDf.columns = ['StockCode', 'Sold Amount']
+			stockCodeList = (list(newDf['StockCode']))
+			soldAmount = (list(newDf['Sold Amount']))
+			fig, ax = plt.subplots(figsize =(140, 45))
+			ax.bar(stockCodeList, soldAmount)
+			ax.set_xlabel('StockCode',fontsize=100)
+			ax.set_ylabel('Sold Amount',fontsize=100)
+			plt.title('Products Distribution of ' + country, fontsize=100)
+			plt.xticks(rotation=90, fontsize=20)
+			plt.yticks(fontsize=50)
+			plt.savefig(path+country+".jpg", dpi=100)
+			plt.cla()
+			plt.clf()
+			plt.close(fig)
+	except Exception as e:
+		print(e)
+		sys.exit("Could not generate the distribution of the products per countries charts")
