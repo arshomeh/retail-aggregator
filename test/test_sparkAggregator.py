0962f4fdef72620a9beaf453d16b3c032b501afa
MEGKRAMPIAN Arsak <arsak.megkrampian@inria.fr>
2021-01-07 16:22:03 +0100

    fix pytest for spartAgg

--- test/test_sparkAggregator.py
+++ test/test_sparkAggregator.py
@@ -147,7 +147,7 @@ def test_PriceDistribution_Pass():
 	real_output = spark.PriceDistribution()
 
 	data = {'UnitPrice': [0.65,0.85,1.25,1.65,1.69,1.85,2.1,2.55,2.75,2.95,3.39,3.75,4.25,4.95,5.95,7.65,7.95,9.95],
-			'Sold Amount': [24.0,60.0,24.0,6.0,32.0,12.0,12.0,6.0,8.0,18.0,18.0,20.0,18.0,12.0,9.0,2.0,8.0,2.0]
+			'Sold Amount': [24.0,60.0,24.0,6.0,32.0,12.0,12.0,6.0,8.0,18.0,18.0,68.0,18.0,12.0,9.0,2.0,8.0,2.0]
 		}
 	expected_output = pd.DataFrame(data, columns = ['UnitPrice', 'Sold Amount'])
 	real_output["UnitPrice"] = pd.to_numeric(real_output["UnitPrice"])
@@ -168,6 +168,6 @@ def test_PriceQuantityRatioPerInvoiceNo_Pass():
 	'{"InvoiceNo":"536367","Ratios":["{\\"Sold Amount\\":8.0,\\"UnitPrice\\":\\"7.95\\",\\"Ratio\\":0.99375}","{\\"Sold Amount\\":12.0,\\"UnitPrice\\":\\"2.1\\",\\"Ratio\\":0.17500000000000002}","{\\"Sold Amount\\":6.0,\\"UnitPrice\\":\\"5.95\\",\\"Ratio\\":0.9916666666666667}","{\\"Sold Amount\\":6.0,\\"UnitPrice\\":\\"4.25\\",\\"Ratio\\":0.7083333333333334}","{\\"Sold Amount\\":2.0,\\"UnitPrice\\":\\"9.95\\",\\"Ratio\\":4.975}","{\\"Sold Amount\\":8.0,\\"UnitPrice\\":\\"3.75\\",\\"Ratio\\":0.46875}","{\\"Sold Amount\\":32.0,\\"UnitPrice\\":\\"1.69\\",\\"Ratio\\":0.0528125}","{\\"Sold Amount\\":3.0,\\"UnitPrice\\":\\"4.95\\",\\"Ratio\\":1.6500000000000001}","{\\"Sold Amount\\":6.0,\\"UnitPrice\\":\\"1.65\\",\\"Ratio\\":0.27499999999999997}"]}',
 	'{"InvoiceNo":"536368","Ratios":["{\\"Sold Amount\\":9.0,\\"UnitPrice\\":\\"4.95\\",\\"Ratio\\":0.55}","{\\"Sold Amount\\":6.0,\\"UnitPrice\\":\\"4.25\\",\\"Ratio\\":0.7083333333333334}"]}',
 	'{"InvoiceNo":"536369","Ratios":["{\\"Sold Amount\\":3.0,\\"UnitPrice\\":\\"5.95\\",\\"Ratio\\":1.9833333333333334}"]}',
-	'{"InvoiceNo":"536370","Ratios":["{\\"Sold Amount\\":24.0,\\"UnitPrice\\":\\"0.65\\",\\"Ratio\\":0.027083333333333334}","{\\"Sold Amount\\":60.0,\\"UnitPrice\\":\\"0.85\\",\\"Ratio\\":0.014166666666666666}","{\\"Sold Amount\\":12.0,\\"UnitPrice\\":\\"3.75\\",\\"Ratio\\":0.3125}","{\\"Sold Amount\\":18.0,\\"UnitPrice\\":\\"2.95\\",\\"Ratio\\":0.1638888888888889}","{\\"Sold Amount\\":24.0,\\"UnitPrice\\":\\"1.25\\",\\"Ratio\\":0.052083333333333336}"]}']
+	'{"InvoiceNo":"536370","Ratios":["{\\"Sold Amount\\":24.0,\\"UnitPrice\\":\\"0.65\\",\\"Ratio\\":0.027083333333333334}","{\\"Sold Amount\\":60.0,\\"UnitPrice\\":\\"0.85\\",\\"Ratio\\":0.014166666666666666}","{\\"Sold Amount\\":60.0,\\"UnitPrice\\":\\"3.75\\",\\"Ratio\\":0.0625}","{\\"Sold Amount\\":18.0,\\"UnitPrice\\":\\"2.95\\",\\"Ratio\\":0.1638888888888889}","{\\"Sold Amount\\":24.0,\\"UnitPrice\\":\\"1.25\\",\\"Ratio\\":0.052083333333333336}"]}']
 
 	assert real_output == expected_output
